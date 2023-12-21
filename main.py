5acc01a03bdba4730ee5491b51fc708e18feafb2
cg117634 <colton11gerth@gmail.com>
2023-12-21 13:43:36 -0600

    added styling and got link logos added in

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pandas as pd
 from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, flash, url_for, json
 
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def home():
     return render_template('pages/index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
