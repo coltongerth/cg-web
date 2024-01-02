50e56ec202c83809a745258b8ecbdf43c5ad46e5
cg117634 <colton11gerth@gmail.com>
2024-01-02 13:30:15 -0600

    added hover over functionality

--- static/js/geomapview.js
+++ static/js/geomapview.js
@@ -158,12 +158,13 @@ var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function getColor(d,y) {
 
-    
-    // d = Number(d)
-    console.log(d)
-    console.log(global_data[y])
     var result = '';
-    if(d <= 0.5 * global_data[y] ){
+    console.log(d)
+    if(d == null){
+        console.log("ping")
+        result = '#000000';
+    }
+    else if(d <= 0.5 * global_data[y] ){
         result = '#2c7bb6';
     }
     else if(d > global_data[y] * 0.5 && d < global_data[y] * .75){
@@ -179,21 +180,36 @@ function getColor(d,y) {
         result = '#d73027'
     }
     
-    // switch(true){
-    //     case(d < global_data[y]):
-    //     result = '#d73027';
-    //     break;
-    //     case(d = global_data[y]):
-    //     result = '#fee090';
-    //     break;
-    //     case(d > global_data[y]):
-    //     result = '#4575b4';
-    //     break;
-    // }
-    console.log(result)
     return  result
             
 }
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (name,props) {
+    if(props != undefined){
+        if(props.yearly_temp != null ){
+            this._div.innerHTML = '<h4>Countries Yearly Change in Temperature</h4>' +  (props ?
+                '<b>' + name + '</b><br />' + props.yearly_temp.toFixed(2) + ' Δ°C '
+                : 'Hover over a country');
+        }
+        else{
+            this._div.innerHTML = '<h4>Countries Yearly Change in Temperature</h4>' +  (props ?
+            '<b>'+ name +'</b><br />Data was not provided for this country': 'Hover over a country');
+        }
+    }
+    
+    
+};
+
+info.addTo(map);
 
 function style(feature) {
     return {
@@ -206,6 +222,14 @@ function style(feature) {
     };
 }
 
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
 function initOrUpdateLayer(json, newYear) {
     // Remove existing choropleth layer if it exists
     if (window.choropleth) {
@@ -215,10 +239,32 @@ function initOrUpdateLayer(json, newYear) {
     year = newYear;
 
     // Add new choropleth layer
-    
-    window.choropleth = L.geoJson(JSON.parse(json[newYear])["features"], { style: style }).addTo(map);
+    window.choropleth = L.geoJson(JSON.parse(json[newYear])["features"], { style: style, onEachFeature: onEachFeature }).addTo(map);
 }
 
+function highlightFeature(e) {
+    var layer = e.target;
+    console.log(layer)
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+    info.update(layer.feature.id,layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    window.choropleth.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
 // Fetch global data and GeoJSON, then initialize the map
 Promise.all([
     fetch('static/data/global_data.json').then(response => response.json()),
