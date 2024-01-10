458f7755daab3b644d2e12a4ad8d2986ffef9bb8
cg117634 <colton11gerth@gmail.com>
2024-01-10 16:01:51 -0600

    massive update to content

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,7 +1,7 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const slidesContainer = document.getElementById("slides-container-fc");
+const slide = document.querySelector(".slide-fc");
+const prevButton = document.getElementById("slide-arrow-prev-fc");
+const nextButton = document.getElementById("slide-arrow-next-fc");
 nextButton.addEventListener("click", () => {
 const slideWidth = slide.clientWidth;
 slidesContainer.scrollLeft += slideWidth;
@@ -9,4 +9,17 @@ slidesContainer.scrollLeft += slideWidth;
 prevButton.addEventListener("click", () => {
 const slideWidth = slide.clientWidth;
 slidesContainer.scrollLeft -= slideWidth;
+});
+
+const slidesContainerHc = document.getElementById("slides-container-hc");
+const slideHc = document.querySelector(".slide-hc");
+const prevButtonHc = document.getElementById("slide-arrow-prev-hc");
+const nextButtonHc = document.getElementById("slide-arrow-next-hc");
+nextButtonHc.addEventListener("click", () => {
+const slideWidthHc = slideHc.clientWidth;
+slidesContainerHc.scrollLeft += slideWidthHc;
+});
+prevButtonHc.addEventListener("click", () => {
+const slideWidthHc = slideHc.clientWidth;
+slidesContainerHc.scrollLeft -= slideWidthHc;
 });
--- static/js/projects.js
+++ static/js/projects.js
@@ -2,12 +2,15 @@ selected_project = ""
 const divs = document.querySelectorAll('.project-link');
 
 divs.forEach(el => el.addEventListener('click', event => {
-        if(selected_project != event.target.innerHTML){
+    console.log(event)
+    console.log(event.target.attributes[1].value)
+    console.log( event.target.attributes[1].value.nodeValue)
+        if(selected_project != event.target.attributes[1].value){
             //Code for specific project
             if(selected_project != ""){
                 document.getElementById(selected_project+"-content").style.display = "none";
             }
-            selected_project = event.target.innerHTML;
+            selected_project = event.target.attributes[1].value;
             document.getElementById("welcome-content").style.display = "none";
             element_selected = document.getElementById(selected_project+"-content");
             element_selected.style.display = element_selected.style.display === 'none' ? '' : 'none';
